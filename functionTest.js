a2cbfd619838122c224637d79a7df467792e763d
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-04-21 03:45:41 +0000

    automatically backed up by learn

--- functionTest.js
+++ functionTest.js
@@ -83,7 +83,7 @@ function removeFromCart(item)  {
 }
 
 function placeOrder(cardNumber)  {
-  if(cardNumber === null )  {
+  if(cardNumber === undefined )  {
     console.log('Sorry, we don\'t have a credit card on file for you.');
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
