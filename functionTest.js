5371dbef548f2952e06993eea3db0574e3ff2d7c
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-04-21 03:35:02 +0000

    automatically backed up by learn

--- functionTest.js
+++ functionTest.js
@@ -84,8 +84,14 @@ function removeFromCart(item)  {
 
 function placeOrder(cardNumber)  {
   if(cardNumber.length === 0)  {
-    console.log()
+    console.log('Sorry, we don\'t have a credit card on file for you.');
+    return 'Sorry, we don\'t have a credit card on file for you.'
   }
+  else  {
+    console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`;
+  }
+  cart.length = 0; //clears cart array
 }
 
 addToCart("Peanut Butter");
